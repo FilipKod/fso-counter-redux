0767ab432d84d5500dd9271361b71d1081b78674
Filip Madunicky <yelidric@gmail.com>
2025-11-23 12:29:11 +0100

    Adds basic Redux implementation
    Adds Redux to manage application state.
    
    This commit introduces a basic Redux setup with a simple counter reducer. It also demonstrates dispatching actions to increment, decrement, and reset the counter, and subscribes to store updates for logging.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
 import App from './App.jsx'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1
+  case 'DECREMENT':
+    return state - 1
+  case 'ZERO':
+    return 0
+  default:
+    return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+})
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'ZERO' })
+store.dispatch({ type: 'DECREMENT' })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
