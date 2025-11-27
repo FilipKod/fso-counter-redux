15c4ecb448a3166fc2e099770a0c3c1569d34302
Filip Madunicky <yelidric@gmail.com>
2025-11-27 19:30:39 +0100

    Migrates to Redux Toolkit
    Migrates the application to use Redux Toolkit for state management.
    
    This simplifies reducer creation and store configuration,
    improving code maintainability.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducers'
-import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducers = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducers)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
